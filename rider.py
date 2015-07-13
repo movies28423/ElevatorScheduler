4bdd7695faa06a97e69dd1f9da142e1f8cc4f72c
movies28423 <movies28423@users.noreply.github.com>
2015-07-13 00:59:27 -0400

    Create rider.py

---
+++ rider.py
@@ -0,0 +1,21 @@
+import ride
+
+class Rider:
+    def __init__(self, rider_name, time_point, starting_point, end_point):
+        self._rider_name = rider_name
+        self._ride_list = [Ride(time_point, starting_point, end_point)]
+
+    def get_rider_name(self):
+        return self._rider_name   
+
+    def get_rider_list(self):
+        return self._ride_list  
+
+    def add_ride(self, time_point, starting_point, end_point):
+        self._ride_list.append(Ride(time_point, starting_point, end_point))
+
+    def remove_ride(self):
+        if len(self._ride_list) > 1:
+            self._ride_list = self._ride_list[1::]
+        else:
+            self._ride_list = []
